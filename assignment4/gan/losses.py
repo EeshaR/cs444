9bc97be3ff90113f026575ea092bbd1a20ffae5f
EeshaR <39881644+EeshaR@users.noreply.github.com>
2024-04-07 16:59:33 -0500

    Update losses.py

--- assignment4/gan/losses.py
+++ assignment4/gan/losses.py
@@ -1,6 +1,5 @@
 import torch
-from torch.nn.functional import binary_cross_entropy_with_logits as bce_loss
-
+from torch.nn.functional import binary_cross_entropy_with_logits as bce_loss, mse_loss
 
 def discriminator_loss(logits_real, logits_fake):
     """
@@ -24,10 +23,13 @@ def discriminator_loss(logits_real, logits_fake):
     #          YOUR CODE HERE          #
     ####################################
 
+    loss_real = bce_loss(logits_real, torch.ones_like(logits_real), reduction='mean')
+    loss_fake = bce_loss(logits_fake, torch.zeros_like(logits_fake), reduction='mean')
+    loss = loss_real + loss_fake
+
     ##########       END      ##########
 
     return loss
-
 
 def generator_loss(logits_fake):
     """
@@ -49,6 +51,8 @@ def generator_loss(logits_fake):
     ####################################
     #          YOUR CODE HERE          #
     ####################################
+
+    loss = bce_loss(logits_fake, torch.ones_like(logits_fake), reduction='mean')
 
     ##########       END      ##########
 
@@ -73,10 +77,13 @@ def ls_discriminator_loss(scores_real, scores_fake):
     #          YOUR CODE HERE          #
     ####################################
 
+    loss_real = 0.5 * torch.mean((scores_real - 1) ** 2)
+    loss_fake = 0.5 * torch.mean(scores_fake ** 2)
+    loss = loss_real + loss_fake
+
     ##########       END      ##########
 
     return loss
-
 
 def ls_generator_loss(scores_fake):
     """
@@ -94,6 +101,56 @@ def ls_generator_loss(scores_fake):
     ####################################
     #          YOUR CODE HERE          #
     ####################################
+
+    loss = 0.5 * torch.mean((scores_fake - 1) ** 2)
+
+    ##########       END      ##########
+
+    return loss
+
+
+def w_discriminator_loss(scores_real, scores_fake):
+    """
+    Compute the Wasserstein GAN loss for the discriminator.
+
+    Inputs:
+    - scores_real: PyTorch Tensor of shape (N,) giving scores for the real data.
+    - scores_fake: PyTorch Tensor of shape (N,) giving scores for the fake data.
+
+    Outputs:
+    - loss: A PyTorch Tensor containing the loss.
+    """
+
+    loss = None
+
+    ####################################
+    #          YOUR CODE HERE          #
+    ####################################
+
+    loss = -torch.mean(scores_real) + torch.mean(scores_fake)
+
+    ##########       END      ##########
+
+    return loss
+
+def w_generator_loss(scores_fake):
+    """
+    Computes the Wasserstein GAN loss for the generator.
+
+    Inputs:
+    - scores_fake: PyTorch Tensor of shape (N,) giving scores for the fake data.
+
+    Outputs:
+    - loss: A PyTorch Tensor containing the loss.
+    """
+
+    loss = None
+
+    ####################################
+    #          YOUR CODE HERE          #
+    ####################################
+
+    loss = -torch.mean(scores_fake)
 
     ##########       END      ##########
 

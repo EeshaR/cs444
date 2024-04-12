e41d10d6a7ff31bb9e7e4c7943b74e9f85695043
EeshaR <39881644+EeshaR@users.noreply.github.com>
2024-04-12 00:04:23 -0500

    Update models.py

--- assignment4/gan/models.py
+++ assignment4/gan/models.py
@@ -5,61 +5,46 @@ class Discriminator(nn.Module):
     def __init__(self, input_channels=3):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            # input is (input_channels) x 64 x 64
             nn.Conv2d(input_channels, 64, 4, stride=2, padding=1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. 64 x 32 x 32
             nn.Conv2d(64, 128, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. 128 x 16 x 16
             nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. 256 x 8 x 8
             nn.Conv2d(256, 512, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. 512 x 4 x 4
             nn.Conv2d(512, 1, 4, stride=1, padding=0, bias=False),
             nn.Sigmoid()
-            # output is 1 x 1 x 1
         )
 
     def forward(self, x):
         x = self.main(x)
-        return x.view(-1, 1).squeeze(1)
+        return x.view(-1)  # Flatten the output
 
 class Generator(nn.Module):
     def __init__(self, noise_dim, output_channels=3):
         super(Generator, self).__init__()
         self.noise_dim = noise_dim
-
         self.main = nn.Sequential(
-            # Input is Z, going into a convolution
             nn.ConvTranspose2d(noise_dim, 512, 4, 1, 0, bias=False),
             nn.BatchNorm2d(512),
             nn.ReLU(True),
-            # State size. 512 x 4 x 4
             nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
-            # State size. 256 x 8 x 8
             nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
-            # State size. 128 x 16 x 16
             nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
-            # State size. 64 x 32 x 32
             nn.ConvTranspose2d(64, output_channels, 4, 2, 1, bias=False),
             nn.Tanh()
-            # Final state size. (output_channels) x 64 x 64
         )
 
     def forward(self, x):
-        # Reshape input noise vector into a batch of images
-        x = x.view(-1, self.noise_dim, 1, 1)  # Reshape to (batch_size, noise_dim, 1, 1)
-        x = self.main(x)
-        return x
+        x = x.view(-1, self.noise_dim, 1, 1)
+        return self.main(x)

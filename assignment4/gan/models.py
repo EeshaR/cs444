bd269ee26846bc3413c89f544686da76ba71c7e6
EeshaR <39881644+EeshaR@users.noreply.github.com>
2024-04-16 14:58:30 -0500

    Update models.py

--- assignment4/gan/models.py
+++ assignment4/gan/models.py
@@ -1,6 +1,14 @@
 import torch
 import torch.nn as nn
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
 class Discriminator(nn.Module):
     def __init__(self, input_channels=3):
         super(Discriminator, self).__init__()
@@ -19,15 +27,15 @@ class Discriminator(nn.Module):
             nn.Conv2d(512, 1, 4, stride=1, padding=0, bias=False),
             nn.Sigmoid()
         )
+        self.main.apply(weights_init)
 
     def forward(self, x):
         x = self.main(x)
-        return x.view(-1)  # Flatten the output
+        return x.view(-1, 1).squeeze(1)  # Flatten the output properly
 
 class Generator(nn.Module):
     def __init__(self, noise_dim, output_channels=3):
         super(Generator, self).__init__()
-        self.noise_dim = noise_dim
         self.main = nn.Sequential(
             nn.ConvTranspose2d(noise_dim, 512, 4, 1, 0, bias=False),
             nn.BatchNorm2d(512),
@@ -44,7 +52,9 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(64, output_channels, 4, 2, 1, bias=False),
             nn.Tanh()
         )
+        self.main.apply(weights_init)
 
     def forward(self, x):
-        x = x.view(-1, self.noise_dim, 1, 1)
+        x = x.view(-1, self.noise_dim, 1, 1)  # Reshape input noise vector into a mini-batch of inputs
         return self.main(x)
+

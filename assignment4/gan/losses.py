6aa7393b07d930c20164d9d16303d1a89d75b907
EeshaR <39881644+EeshaR@users.noreply.github.com>
2024-04-16 23:12:40 -0500

    Update losses.py

--- assignment4/gan/losses.py
+++ assignment4/gan/losses.py
@@ -1,9 +1,16 @@
 import torch
 from torch.nn.functional import binary_cross_entropy_with_logits as bce_loss
+import torch.nn.functional as F
+import torch.nn as nn
+
 
 def discriminator_loss(logits_real, logits_fake):
     """
-    Computes the discriminator loss using binary cross entropy with logits.
+    Computes the discriminator loss.
+
+    You should use the stable torch.nn.functional.binary_cross_entropy_with_logits
+    loss rather than using a separate softmax function followed by the binary cross
+    entropy loss.
 
     Inputs:
     - logits_real: PyTorch Tensor of shape (N,) giving scores for the real data.
@@ -12,18 +19,33 @@ def discriminator_loss(logits_real, logits_fake):
     Returns:
     - loss: PyTorch Tensor containing (scalar) the loss for the discriminator.
     """
-    true_labels = torch.ones_like(logits_real)
-    false_labels = torch.zeros_like(logits_fake)
 
-    loss_real = bce_loss(logits_real, true_labels)
-    loss_fake = bce_loss(logits_fake, false_labels)
+    loss = None
 
-    loss = (loss_real + loss_fake) / 2
+    ####################################
+    #          YOUR CODE HERE          #
+    ####################################
+
+    ##########       END      ##########
+
+    # using use the stable torch.nn.functional.binary_cross_entropy_with_logits 
+    lossr = F.binary_cross_entropy_with_logits(logits_real, torch.ones_like(logits_real))
+    lossf = F.binary_cross_entropy_with_logits(logits_fake, torch.zeros_like(logits_fake))
+
+    loss = (lossr + lossf) / 2
     return loss
+
+    # referenced - 
+    # https://stackoverflow.com/questions/68607705/binary-cross-entropy-with-logits-produces-negative-output
+
 
 def generator_loss(logits_fake):
     """
-    Computes the generator loss using binary cross entropy with logits.
+    Computes the generator loss.
+
+    You should use the stable torch.nn.functional.binary_cross_entropy_with_logits
+    loss rather than using a separate softmax function followed by the binary cross
+    entropy loss.
 
     Inputs:
     - logits_fake: PyTorch Tensor of shape (N,) giving scores for the fake data.
@@ -31,9 +53,23 @@ def generator_loss(logits_fake):
     Returns:
     - loss: PyTorch Tensor containing the (scalar) loss for the generator.
     """
-    true_labels = torch.ones_like(logits_fake)
-    loss = bce_loss(logits_fake, true_labels)
+
+    loss = None
+
+    ####################################
+    #          YOUR CODE HERE          #
+    ####################################
+
+    ##########       END      ##########
+
+    # use the stable torch.nn.functional.binary_cross_entropy_with_logits 
+    loss = torch.nn.functional.binary_cross_entropy_with_logits(logits_fake, (torch.ones_like(logits_fake)))
     return loss
+
+    # referenced - 
+    # https://stackoverflow.com/questions/74900046/custom-bce-loss-giving-undefined-results
+    # https://ai.stackexchange.com/questions/40699/loss-is-negative-dqn-with-bce-loss-function
+
 
 def ls_discriminator_loss(scores_real, scores_fake):
     """
@@ -46,10 +82,22 @@ def ls_discriminator_loss(scores_real, scores_fake):
     Outputs:
     - loss: A PyTorch Tensor containing the loss.
     """
-    loss_real = torch.mean((scores_real - 1) ** 2)
-    loss_fake = torch.mean(scores_fake ** 2)
-    loss = 0.5 * (loss_real + loss_fake)
+
+    loss = None
+
+    ####################################
+    #          YOUR CODE HERE          #
+    ####################################
+
+    ##########       END      ##########
+
+    loss = 0.5 * ((F.mse_loss(scores_real, torch.ones_like(scores_real))) + (F.mse_loss(scores_fake, torch.zeros_like(scores_fake))))
     return loss
+
+    # referenced - 
+    # https://stackoverflow.com/questions/72274562/mseloss-from-pytorch
+    # https://stackoverflow.com/questions/78154131/evaluation-metrics-of-mse-mae-and-rmse
+    
 
 def ls_generator_loss(scores_fake):
     """
@@ -61,5 +109,15 @@ def ls_generator_loss(scores_fake):
     Outputs:
     - loss: A PyTorch Tensor containing the loss.
     """
-    loss = 0.5 * torch.mean((scores_fake - 1) ** 2)
+
+    loss = None
+
+    ####################################
+    #          YOUR CODE HERE          #
+    ####################################
+
+    ##########       END      ##########
+
+    # L = 0.5 × mean((scores_fake)^2)
+    loss = 0.5 * torch.mean(torch.pow(scores_fake - 1, 2))
     return loss

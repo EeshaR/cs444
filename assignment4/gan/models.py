2c2601a8b31d5986a5401aff5be61308fb092203
EeshaR <39881644+EeshaR@users.noreply.github.com>
2024-04-16 23:10:59 -0500

    Update models.py

--- assignment4/gan/models.py
+++ assignment4/gan/models.py
@@ -1,71 +1,61 @@
 import torch
-from gan.spectral_normalization import SpectralNorm
 import torch.nn as nn
-import torch.nn.functional as F
 
-class Discriminator(torch.nn.Module):
+class Discriminator(nn.Module):
     def __init__(self, input_channels=3):
         super(Discriminator, self).__init__()
-        
-        #Hint: Hint: Apply spectral normalization to convolutional layers. Input to SpectralNorm should be your conv nn module
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-        self.conv1 = SpectralNorm(nn.Conv2d(3, 128, kernel_size = 4, stride = 2, padding = 1))
-        self.conv2 = SpectralNorm(nn.Conv2d(128, 256, kernel_size = 4, stride = 2, padding = 1))
-        self.bn1 = nn.BatchNorm2d(256)
-        self.conv3 = SpectralNorm(nn.Conv2d(256, 512, kernel_size = 4, stride = 2, padding = 1))
-        self.bn2 = nn.BatchNorm2d(512)
-        self.conv4 = SpectralNorm(nn.Conv2d(512, 1024, kernel_size = 4, stride = 2, padding = 1))
-        self.bn3 = nn.BatchNorm2d(1024)
-        self.conv5 = SpectralNorm(nn.Conv2d(1024, 1, kernel_size = 4, stride = 1, padding = 1))
-        self.leaky_relu = nn.LeakyReLU(0.2)
-        ##########       END      ##########
-    
+        self.main = nn.Sequential(
+            nn.Conv2d(input_channels, 128, 4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+           
+            nn.Conv2d(128, 256, 4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+           
+            nn.Conv2d(256, 512, 4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 1024, 4, stride=2, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(1024, 1, 4, stride=1, padding=0),
+        )
+
     def forward(self, x):
-        
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-        x = self.leaky_relu(self.conv1(x))
-        x = self.leaky_relu(self.bn1(self.conv2(x)))
-        x = self.leaky_relu(self.bn2(self.conv3(x)))
-        x = self.leaky_relu(self.bn3(self.conv4(x)))
-        x = self.conv5(x)
-        ##########       END      ##########
-        
-        return x
-
-
-class Generator(torch.nn.Module):
+        # return self.main(x)
+        x = self.main(x)
+        return x.view(-1)
+       
+class Generator(nn.Module):
     def __init__(self, noise_dim, output_channels=3):
-        super(Generator, self).__init__()    
+        super(Generator, self).__init__()
         self.noise_dim = noise_dim
-        
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-        self.conv1 = nn.ConvTranspose2d(self.noise_dim, 1024, kernel_size = 4, stride = 1)
-        self.bn1 = nn.BatchNorm2d(1024)
-        self.conv2 = nn.ConvTranspose2d(1024, 512, kernel_size = 4, stride = 2, padding = 1)
-        self.bn2 = nn.BatchNorm2d(512)
-        self.conv3 = nn.ConvTranspose2d(512, 256, kernel_size = 4, stride = 2, padding = 1)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.conv4 = nn.ConvTranspose2d(256, 128, kernel_size = 4, stride = 2, padding = 1)
-        self.bn4 = nn.BatchNorm2d(128)
-        self.conv5 = nn.ConvTranspose2d(128, 3, kernel_size = 4, stride = 2, padding = 1)
-        ##########       END      ##########
-    
+        self.output_channels = output_channels
+        self.main = nn.Sequential(
+           
+            nn.ConvTranspose2d(self.noise_dim, 1024, 4, 1, 0),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+           
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+           
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+           
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+           
+            nn.ConvTranspose2d(128, self.output_channels, 4, 2, 1),
+            nn.Tanh()
+        )
+
     def forward(self, x):
-        
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = torch.tanh(self.conv5(x))
-        ##########       END      ##########
-        
+        x = x.view(-1, self.noise_dim, 1, 1)
+        x = self.main(x)
         return x

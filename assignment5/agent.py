b085c4f322ad0a26b4e4f0d9c1eace35ae5cab34
EeshaR <39881644+EeshaR@users.noreply.github.com>
2024-04-26 18:07:20 -0500

    Update agent.py

--- assignment5/agent.py
+++ assignment5/agent.py
@@ -42,11 +42,11 @@ class Agent():
 
     """Get action using policy net using epsilon-greedy policy"""
     def get_action(self, state):
-    if np.random.rand() <= self.epsilon:
-        return torch.tensor([random.randrange(self.action_size)], device=device)  # Ensure it's a tensor
-    else:
-        with torch.no_grad():
-            state = state.unsqueeze(0)  # Add batch dimension if not already added
+        if np.random.rand() <= self.epsilon:
+            return torch.tensor([random.randrange(self.action_size)], device=device)  # Ensure it's a tensor
+        else:
+            with torch.no_grad():
+                state = state.unsqueeze(0)  # Add batch dimension if not already added
             q_values = self.policy_net(state)
             return q_values.max(1)[1]  # This is already a tensor
 
